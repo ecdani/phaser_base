cf9a6ea05296eba9156df9c51cd8f06ce48c3738
Dani <ec.dani@gmail.com>
2022-10-31 17:29:26 +0100

    Adds tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-'use client';
-import Game from "../components/game.js";
+import '../styles/globals.css'
 
 export default function RootLayout({ children }) {
     return (
@@ -8,7 +7,7 @@ export default function RootLayout({ children }) {
           <title>Nesxt.js</title>
           <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
         </head>
-        <body>{children} <Game /></body>
+        <body>{children}</body>
       </html>
     );
   }
