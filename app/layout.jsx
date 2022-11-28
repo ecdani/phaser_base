a2e8c65c6d8b2c3f7efde8c8bb890d08d20ad5b0
Florian <fvgarcia@dminc.com>
2022-11-28 14:29:38 +0100

    Add meta with viewport for mobile devices

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>Next.js</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js" />
       </head>
       <body>{children}</body>
